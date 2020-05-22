59ec7cf2dc2f29462309d97302a38c4dbe849a35
Bik Nandy <bikramn123@gmail.com>
2020-05-21 17:56:33 -0700

    Added working Google maps

---
+++ app/src/main/java/edu/ucsb/cs/cs184/oddjobs/ui/map/MapFragment.java
@@ -0,0 +1,96 @@
+package edu.ucsb.cs.cs184.oddjobs.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.*;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import edu.ucsb.cs.cs184.oddjobs.R;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+
+    private MapViewModel mapViewModel;
+    private SupportMapFragment mMapView;
+    private GoogleMap googleMap;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        mapViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mMapView = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(this);
+
+
+
+        //final TextView textView = root.findViewById(R.id.text_home);
+//        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap gmap) {
+        googleMap = gmap;
+
+        LatLng ucsb = new LatLng(34.412936, -119.847863);
+        googleMap.addMarker(new MarkerOptions().position(ucsb)
+                .title("Marker in IV"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ucsb));
+    }
+}
--- app/src/main/java/edu/ucsb/cs/cs184/oddjobs/ui/map/MapViewModel.java
+++ app/src/main/java/edu/ucsb/cs/cs184/oddjobs/ui/map/MapViewModel.java
@@ -1,16 +1,16 @@
-package edu.ucsb.cs.cs184.oddjobs.ui.home;
+package edu.ucsb.cs.cs184.oddjobs.ui.map;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class MapViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public MapViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Test");
     }
 
     public LiveData<String> getText() {
